ba6039d9cd508b7057bc15d06a8c480c5ce9eb0d
rain4ky <995608632@qq.com>
2023-06-06 19:25:56 +0800

    update

--- 4/im.py
+++ 4/im.py
@@ -19,18 +19,28 @@ for i in range(0,len(result)):
     urls.append("https://www.zjcs.net.cn/xinwenwang/"+result[i][0])
     titles.append(result[i][1])
     dates.append(result[i][2])
-for i in range(ed,690,-1):
+for i in range(ed-1,693,-1):
     #虽然只是到690，但是可以改成1，爬取全部数据
     urll="https://www.zjcs.net.cn/xinwenwang/zhxw/{}.htm".format(i)
     resp=requests.get(urll,headers=headers)
     resp.encoding="utf-8"
     content=resp.text
-    pattern=re.compile( r'<dd id="line_u10_.?".*?>.*?<a href="../(.*?)" class="fl">(.*?)</a>.*?<span class="fr gray">(.*?)</span>.*?</dd>',re.S)
+    pattern=re.compile( r'<dd id="line_u10_.?".*?>.*?<a href="../(.*?).htm" class="fl".*?>(.*?)</a>.*?<span class="fr gray">(.*?)</span>.*?</dd>',re.S)
     result=pattern.findall(content)
+    print(urll)
     for i in range(0,len(result)):
-        urls.append("https://www.zjcs.net.cn/xinwenwang/"+result[i][0])
+        urln="https://www.zjcs.net.cn/xinwenwang/"+result[i][0]+".htm"
+        print("urln="+urln)
+        urls.append(urln)
         titles.append(result[i][1])
         dates.append(result[i][2])
-for i in range(0,len(urls)):
-    #这里只输出了新闻网页的url，但是修改输出都挺方便的
-    print(urls[i])
+#做了一点小修改，但是关于具体新闻内容，好像用正则不太好爬啊，所以没写完
+contents=[]
+for u in urls:
+    resp=requests.get(u,headers=headers)
+    resp.encoding="utf-8"
+    content=resp.text
+    pattern=re.compile( r'<div>.*?<p>(.*?)</p>.*?</div>',re.S)
+    result=pattern.findall(content)
+    contents.append(result)
+print(contents[1])
